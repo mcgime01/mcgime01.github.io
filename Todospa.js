0ed6f2e435061deb9c23a3ec8d538c2afb46c645
mcgime01 <mcgime01@luther.edu>
2015-10-20 14:06:05 -0500

    Update Todospa.js

--- Todospa.js
+++ Todospa.js
@@ -2,6 +2,7 @@ addnew = function () {
   var myList;
   myList = document.querySelector('#myList');
   myImput= document.querySelector('#myImput');
+  myList.innerHTML=myInput.value
   li = document.createElement("li")
   
 }
