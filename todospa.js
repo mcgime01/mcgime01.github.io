e5135aec081a32344be4bc84a8125e4dffbad1c9
mcgime01 <mcgime01@luther.edu>
2015-10-22 00:50:23 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -14,7 +14,7 @@ addnew = function () {
   
   select = document.querySelector('#priority')
   className = select.value
-  console.log ("hello world")
+  console.log (className)
 }
 
 checkFunc = function () {
